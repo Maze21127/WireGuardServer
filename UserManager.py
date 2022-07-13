631641662db73f2b47e1458995f2102f788eb7b8
Maze <invoker322sf@gmail.com>
2022-07-13 20:32:06 +1000

    some fixes

--- UserManager.py
+++ UserManager.py
@@ -78,10 +78,11 @@ class UserManager:
         print("Пользователь создан")
         print(self._user)
         #self._create_user_config()
-        self._reformat_config_file()
-        print("Файл конфигураций обновлен")
+        #self._reformat_config_file()
+        #print("Файл конфигураций обновлен")
         #self._add_user_to_config()
         self._database.create_new_config(self._user, tg_id)
+        return
         self.update_wireguard()
         return self.create_user_config_by_name(config_name, tg_id)
 
